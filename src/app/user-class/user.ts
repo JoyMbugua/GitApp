db188e2f9ba7d1a1893edd3304dffa3f1730b5d7
JoyMbugua <wacumbugua@gmail.com>
2021-02-08 09:40:35 +0300

    created a user class

---
+++ src/app/user-class/user.ts
@@ -0,0 +1,17 @@
+export class User {
+    
+    constructor(
+        public login: string, 
+        public url: string,
+        public avatar_url: string,
+        public public_repos: string, 
+        public public_gists: string, 
+        public followers: number, 
+        public following: number,
+        public email: string,
+        public location: string,
+        public blog: string,
+        public created_at: string
+        ){}
+
+}
